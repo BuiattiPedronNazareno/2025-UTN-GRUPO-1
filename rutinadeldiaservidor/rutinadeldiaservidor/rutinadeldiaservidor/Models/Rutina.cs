2d4de6a7d0c542f5ffdae479a5ac99f87fdea60e
fpianelli <fpianelli@alu.frlp.utn.edu.ar>
2025-09-08 15:17:06 -0300

    feat: agregar modelo base de rutina

---
+++ rutinadeldiaservidor/rutinadeldiaservidor/rutinadeldiaservidor/Models/Rutina.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace rutinadeldiaservidor.Models
+{
+    public class Rutina
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public string Estado { get; set; } = "Activa";
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+        public string Imagen { get; set; }
+    }
+}
