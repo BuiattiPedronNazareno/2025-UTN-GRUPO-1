01c021d11988305accca9d346f96a27e410d042c
fpianelli <fpianelli@alu.frlp.utn.edu.ar>
2025-09-08 15:24:41 -0300

    feat: agregar dbcontext y conexion a bd

---
+++ rutinadeldiaservidor/rutinadeldiaservidor/rutinadeldiaservidor/Data/RutinaContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using rutinadeldiaservidor.Models;
+using System;
+
+namespace rutinadeldiaservidor.Data
+{
+    public class RutinaContext : DbContext
+    {
+        public RutinaContext(DbContextOptions<RutinaContext> options) : base(options) { }
+        public DbSet<Rutina> Rutinas { get; set; }
+    }
+}
